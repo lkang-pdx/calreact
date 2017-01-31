f4908f06271282011f241472359595d6fec58921
Luna Kang <luna.kang.ca@gmail.com>
2017-01-31 15:03:55 -0800

    Appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,16 @@
 class AppointmentsController < ApplicationController
   def index
+    @appointments = Appointment.order('appt_time ASC')
+    @appointment = Appointment.new
+  end
+
+  def create
+    @appointment = Appointment.create(appointment_params)
+    redirect_to :root
+  end
+
+  private
+  def appointment_params
+    params.require(:appointment).permit(:title, :appt_time)
   end
 end
